cf5cf2460ff20a70625704c7187b9d26ce5f9269
Benny Johansson <benny.json@gmail.com>
2018-09-08 15:51:50 +0100

    Basic example bar chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { Bar } from 'react-chartjs-2';
 
 class App extends Component {
   state = {
-    response: ''
+    response: '',
+    chartData: null
   };
 
   componentDidMount() {
+    this.setState({
+      chartData: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      }
+    });
+
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => {})
       .catch(err => console.log(err));
   }
 
@@ -25,14 +55,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          {this.state.response}
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { this.state.chartData ?
+          <div className="chart-container">
+              <Bar data={this.state.chartData}
+                options={{
+                  responsive: true,
+                  maintainAspectRatio: false
+                }
+              }/> 
+          </div>
+          :
+          <p>Loading...</p>
+        }
       </div>
     );
   }
