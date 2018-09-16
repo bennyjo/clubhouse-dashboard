584f0cef119619bbaf0d87f641b262d02a72037a
Benny Johansson <benny.json@gmail.com>
2018-09-16 21:45:59 +0100

    Add more sprints

--- client/src/App.js
+++ client/src/App.js
@@ -57,6 +57,51 @@ class App extends Component {
               );
 
             const sprints = {
+              'q1s5': {
+                duration: [new Date('2018-03-07T00:00:00Z'), new Date('2018-03-20T00:00:00Z')],
+                cycleTimeSum: 0,
+                cycleTimeCount: 0
+              },
+              'q1s6': {
+                duration: [new Date('2018-03-21T00:00:00Z'), new Date('2018-04-03T00:00:00Z')],
+                cycleTimeSum: 0,
+                cycleTimeCount: 0
+              },
+              'q2s1': {
+                duration: [new Date('2018-04-04T00:00:00Z'), new Date('2018-04-17T00:00:00Z')],
+                cycleTimeSum: 0,
+                cycleTimeCount: 0
+              },
+              'q2s2': {
+                duration: [new Date('2018-04-18T00:00:00Z'), new Date('2018-05-01T00:00:00Z')],
+                cycleTimeSum: 0,
+                cycleTimeCount: 0
+              },
+              'q2s3': {
+                duration: [new Date('2018-05-02T00:00:00Z'), new Date('2018-05-15T00:00:00Z')],
+                cycleTimeSum: 0,
+                cycleTimeCount: 0
+              },
+              'q2s4': {
+                duration: [new Date('2018-05-16T00:00:00Z'), new Date('2018-05-29T00:00:00Z')],
+                cycleTimeSum: 0,
+                cycleTimeCount: 0
+              },
+              'q2s5': {
+                duration: [new Date('2018-05-30T00:00:00Z'), new Date('2018-06-12T00:00:00Z')],
+                cycleTimeSum: 0,
+                cycleTimeCount: 0
+              },
+              'q2s6': {
+                duration: [new Date('2018-06-13T00:00:00Z'), new Date('2018-06-26T00:00:00Z')],
+                cycleTimeSum: 0,
+                cycleTimeCount: 0
+              },
+              'q2s7': {
+                duration: [new Date('2018-06-27T00:00:00Z'), new Date('2018-07-10T00:00:00Z')],
+                cycleTimeSum: 0,
+                cycleTimeCount: 0
+              },
               'q3s1': {
                 duration: [new Date('2018-07-11T00:00:00Z'), new Date('2018-07-24T00:00:00Z')],
                 cycleTimeSum: 0,
